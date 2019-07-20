f1ff046f8062b6a421c62a091a89ed1cb619c35c
miftahunajat <miftahunajat@gmail.com>
2019-07-21 02:07:54 +0700

    mandiri mission

--- app/src/main/java/com/example/mandirionline/adapter/ProductAdapter.java
+++ app/src/main/java/com/example/mandirionline/adapter/ProductAdapter.java
@@ -11,13 +11,14 @@ import com.example.mandirionline.R;
 import com.example.mandirionline.network.model.ProductResponse;
 import com.squareup.picasso.Picasso;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
     private List<ProductResponse> mDataset;
 
     public static class ProductViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         private ImageView imageView;
         public TextView titleProduct;
         public TextView subTitleProduct;
@@ -26,34 +27,28 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         public ProductViewHolder(View v) {
             super(v);
             imageView = (ImageView)  v.findViewById(R.id.image_product);
-            titleProduct = (TextView) v.findViewById(R.id.title_product);
-            subTitleProduct = (TextView) v.findViewById(R.id.subtitle_product);
-            validDate = (TextView) v.findViewById(R.id.valid_date);
-            nameMerchant = (TextView) v.findViewById(R.id.name_merchant);
+            titleProduct = v.findViewById(R.id.title_product);
+            subTitleProduct = v.findViewById(R.id.subtitle_product);
+            validDate = v.findViewById(R.id.valid_date);
+            nameMerchant = v.findViewById(R.id.name_merchant);
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public ProductAdapter(List<ProductResponse> myDataset) {
         mDataset = myDataset;
     }
 
-    // Create new views (invoked by the layout manager)
+    @NotNull
     @Override
-    public ProductViewHolder onCreateViewHolder(ViewGroup parent,
+    public ProductViewHolder onCreateViewHolder(@NotNull ViewGroup parent,
                                                 int viewType) {
-        // create a new view
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_product, parent, false);
-        ProductViewHolder vh = new ProductViewHolder(v);
-        return vh;
+        return new ProductViewHolder(v);
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ProductViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
+    public void onBindViewHolder(ProductViewHolder  holder, int position) {
         ProductResponse product = mDataset.get(position);
         Picasso.get().load(product.getImageUrl().getUrl()).into(holder.imageView);
         holder.titleProduct.setText(product.getTitleProduct());
@@ -63,7 +58,6 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mDataset.size();
---
+++ app/src/main/java/com/example/mandirionline/ui/main/PlaceholderFragment.java
@@ -0,0 +1,65 @@
+package com.example.mandirionline.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+
+import com.example.mandirionline.R;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlaceholderFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private PageViewModel pageViewModel;
+
+    public static PlaceholderFragment newInstance(int index) {
+        PlaceholderFragment fragment = new PlaceholderFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pageViewModel = ViewModelProviders.of(this).get(PageViewModel.class);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+        }
+        pageViewModel.setIndex(index);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        Spinner spinner = (Spinner) root.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.nominal_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        Spinner spinner2 = (Spinner) root.findViewById(R.id.sp_weekly_monthly);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getContext(),
+                R.array.weekly_monthly, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+        return root;
+    }
+}
