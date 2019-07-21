ef03a485be98cc56ffd821469323aab98cf6b63d
sambasjundi <sambas14051995@gmail.com>
2019-07-21 08:07:58 +0700

    page new

--- app/src/main/java/com/example/mandirionline/MainActivity.java
+++ app/src/main/java/com/example/mandirionline/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.mandirionline;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.mandirionline.fragment.HomeFragment;
+import com.example.mandirionline.fragment.OtherFragment;
+import com.example.mandirionline.fragment.PortofolioFragment;
+import com.example.mandirionline.fragment.ProductFragment;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
     }
+
 }
